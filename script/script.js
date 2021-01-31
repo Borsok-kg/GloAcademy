b23984f7bc7777b4f7fae4aaae8bfe970292a61d
Azat Tagaev <braza@yandex.ru>
2021-01-31 11:51:23 +0600

    Первый коммит

---
+++ script/script.js
@@ -0,0 +1,12 @@
+const money = 'rub',
+      mission = 'buying a house',
+      period = '1 month';
+
+let income = 20000,
+    addExpenses = 18000,
+    deposit = 2000;
+
+alert('Что то мало!');
+
+console.log('Деваться не куда!');
+
