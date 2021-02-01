c91d9f846a477b15c2345fa6d4fb88d5ef6be39f
Azat Tagaev <braza@yandex.ru>
2021-02-01 16:33:31 +0600

    Исправления урок 2

--- script/script.js
+++ script/script.js
@@ -1,14 +1,14 @@
 let money = 15000,
-    income = 30000,
+    income = 'Freelance',
     addExpenses = 'Интернет 1000, коммуналка 5000, развлечения 10000, плюшки 10000',
     deposit = true,
     mission = 2000000,
     period = 12,
     budgetDay = 45000 / 30;
 
-console.log(money);
-console.log(income);
-console.log(deposit);
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
 console.log(addExpenses.length);
 console.log(`Период равен ${period} месяцев`);
 console.log(`Цель зарабоать ${mission} рублей`);
